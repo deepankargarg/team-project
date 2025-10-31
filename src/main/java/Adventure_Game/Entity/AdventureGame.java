03884aa2ccec5f8f05d7ccff0c9a73b5e8b1745d
lemonknife <lemonife@protonmail.com>
2025-10-31 17:53:24 -0400

    feat: add initial AdventureGame class scaffolding

---
+++ src/main/java/Adventure_Game/Entity/AdventureGame.java
@@ -0,0 +1,23 @@
+package Adventure_Game.Entity;
+
+import Battle_System.User.Monster;
+import Battle_System.User.User;
+
+import java.util.List;
+
+public class AdventureGame {
+
+    private User user;
+    private List<Monster> monsters;
+
+    private List<List<Location>> locationMap;
+    private List<Location> locationSequence;
+
+    public void writeSaveFile() {
+        // TODO: write save data
+    }
+
+    public void readSaveFile() {
+        // TODO: read save data
+    }
+}
