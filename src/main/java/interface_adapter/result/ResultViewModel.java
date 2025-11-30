d6583e597c0ba51bad5af14863f0eae4daae6726
deepankargarg <gargdeepankar0202@gmail.com>
2025-11-30 04:40:21 -0500

    Added a view model

--- src/main/java/interface_adapter/result/ResultViewModel.java
+++ src/main/java/interface_adapter/result/ResultViewModel.java
@@ -1,25 +1,39 @@
 package interface_adapter.result;
+import interface_adapter.results.ResultsState;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ResultViewModel {
-    private String message = "";
-    private final List<Runnable> listeners = new ArrayList<>();
+//    private String message = "";
+//    private final List<Runnable> listeners = new ArrayList<>();
+//
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//        notifyListeners();
+//    }
+//
+//    public void addListener(Runnable listener) {
+//        listeners.add(listener);
+//    }
+//
+//    private void notifyListeners() {
+//        for (Runnable r : listeners) r.run();
+//    }
 
-    public String getMessage() {
-        return message;
+    public static final String TITLE_LABEL = "Game Results";
+    public static final String BACK_BUTTON_LABEL = "Play Again";
+
+    public ResultViewModel() {
+        //super("results");
+        setState(new ResultsState());
     }
 
-    public void setMessage(String message) {
-        this.message = message;
-        notifyListeners();
-    }
-
-    public void addListener(Runnable listener) {
-        listeners.add(listener);
-    }
-
-    private void notifyListeners() {
-        for (Runnable r : listeners) r.run();
+    private void setState(ResultsState resultsState) {
+        //
     }
 }
